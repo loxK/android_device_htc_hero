427f93503b4017649e442036d235291631d62f60
Lox <lox.dev@knc.nc>
2010-07-01 12:45:47 +1100

    Inherit from device_dream_sapphire.mk

--- device_hero.mk
+++ device_hero.mk
@@ -23,44 +23,9 @@ PRODUCT_PACKAGES := \
 # proprietary side of the device
 $(call inherit-product-if-exists, vendor/htc/hero/device_hero-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS := device/htc/hero/overlay
-
-# Install the features available on this device.
-PRODUCT_COPY_FILES += \
-    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
-    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml
-
-PRODUCT_PROPERTY_OVERRIDES := \
-    ro.media.dec.jpeg.memcap=10000000
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    rild.libpath=/system/lib/libhtc_ril.so \
-    wifi.interface=tiwlan0
-
-# Time between scans in seconds. Keep it high to minimize battery drain.
-# This only affects the case in which there are remembered access points,
-# but none are in range.
-PRODUCT_PROPERTY_OVERRIDES += \
-    wifi.supplicant_scan_interval=15
-
-# density in DPI of the LCD of this board. This is used to scale the UI
-# appropriately. If this property is not defined, the default value is 160 dpi. 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=160
-
-# Default network type
-# 0 => WCDMA Preferred.
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.default_network=0
-
-# media configuration xml file
-PRODUCT_COPY_FILES += \
-    device/htc/hero/media_profiles.xml:/system/etc/media_profiles.xml
+# bits that are shared between dream and sapphire
+$(call inherit-product, device/htc/dream-sapphire/device_dream_sapphire.mk)
 
 # stuff common to all HTC phones
-$(call inherit-product, device/htc/common/common.mk)
+$(call inherit-product, device/htc/common/common_small.mk)
 
