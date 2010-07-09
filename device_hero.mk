b579dd9d40caf744133d17d05ae4faef00888b46
Lox <lox.dev@knc.nc>
2010-07-09 17:49:13 +1100

    Was overriding vendor/htc/hero/device_hero-vendor.mk

--- device_hero.mk
+++ device_hero.mk
@@ -33,7 +33,7 @@ PRODUCT_PACKAGES := \
 $(call inherit-product-if-exists, vendor/htc/hero/device_hero-vendor.mk)
 
 # from device_dream_sapphire.mk
-PRODUCT_COPY_FILES := \
+PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/base/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
     frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
