71e8d90a8bbe184bafe09116af05aff0879f9f1a
Alex Boag-Munroe <boagenator@gmail.com>
2010-08-29 03:32:42 +0100

    Fix lights driver so all notifications work properly.

--- liblights/lights.c
+++ liblights/lights.c
@@ -37,20 +37,23 @@
 static pthread_once_t g_init = PTHREAD_ONCE_INIT;
 static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
 static struct light_state_t g_notify;
-static struct light_state_t g_attention;
+static int g_attention = 0;
 static struct light_state_t g_no_action;
 static struct light_state_t g_battery;
+static int g_haveAmberLed = 0;
 static int g_backlight = 255;
+static int g_trackball = -1;
+static int g_haveTrackballLight = 0;
 
 char const*const AMBER_LED_FILE = "/sys/class/leds/amber/brightness";
 char const*const GREEN_LED_FILE = "/sys/class/leds/green/brightness";
-char const*const JOGBALL_FILE = "/sys/class/leds/jogball-backlight/brightness";
+char const*const TRACKBALL_FILE = "/sys/class/leds/jogball-backlight/brightness";
 
 char const*const BUTTON_FILE = "/sys/class/leds/button-backlight/brightness";
 
 char const*const AMBER_BLINK_FILE = "/sys/class/leds/amber/blink";
 char const*const GREEN_BLINK_FILE = "/sys/class/leds/green/blink";
-char const*const JOGBALL_BLINK_FILE = "/sys/class/leds/jogball-backlight/blink";
+char const*const TRACKBALL_BLINK_FILE = "/sys/class/leds/jogball-backlight/blink";
 
 char const*const LCD_BACKLIGHT_FILE = "/sys/class/leds/lcd-backlight/brightness";
 
@@ -71,7 +74,7 @@ static int write_int (const char* path, int value) {
 	fd = open(path, O_RDWR);
 	if (fd < 0) {
 		if (already_warned == 0) {
-			LOGE("write_int failed to open %s\n", path);
+			LOGV("write_int failed to open %s\n", path);
 			already_warned = 1;
 		}
 		return -errno;
@@ -88,6 +91,8 @@ static int write_int (const char* path, int value) {
 
 void init_globals (void) {
 	pthread_mutex_init (&g_lock, NULL);
+	g_haveTrackballLight = (access(TRACKBALL_FILE, W_OK) == 0) ? 1 : 0;
+	g_haveAmberLed = (access(AMBER_LED_FILE, W_OK) == 0) ? 1 : 0;
 }
 
 static int is_lit (struct light_state_t const* state) {
@@ -119,14 +124,15 @@ static void set_speaker_light_locked (struct light_device_t *dev, struct light_s
 					write_int (GREEN_BLINK_FILE, 1);
 					break;
 				case LED_WHITE:
-					write_int (JOGBALL_BLINK_FILE, 1);
+					write_int (TRACKBALL_BLINK_FILE, 1);
 					break;
 				case LED_BLANK:
 					write_int (AMBER_BLINK_FILE, 0);
 					write_int (GREEN_BLINK_FILE, 0);
+					write_int (TRACKBALL_BLINK_FILE, 0);
 					break;
 				default:
-					LOGE("set_led_state colorRGB=%08X, unknown color\n",
+					LOGV("set_led_state colorRGB=%08X, unknown color\n",
 							colorRGB);
 					break;
 			}
@@ -144,17 +150,18 @@ static void set_speaker_light_locked (struct light_device_t *dev, struct light_s
 				case LED_WHITE:
 					write_int (AMBER_LED_FILE, 0);
 					write_int (GREEN_LED_FILE, 0);
-					write_int (JOGBALL_FILE, 1);
+					write_int (TRACKBALL_FILE, 1);
 					break;
 				case LED_BLANK:
 					write_int (AMBER_LED_FILE, 0);
 					write_int (GREEN_LED_FILE, 0);
+					write_int (TRACKBALL_FILE, 0);
 					break;
 
 			}
 			break;
 		default:
-			LOGE("set_led_state colorRGB=%08X, unknown mode %d\n",
+			LOGV("set_led_state colorRGB=%08X, unknown mode %d\n",
 					colorRGB, state->flashMode);
 	}
 
@@ -174,10 +181,31 @@ static int set_light_buttons (struct light_device_t* dev,
 	int on = is_lit (state);
 	pthread_mutex_lock (&g_lock);
 	err = write_int (BUTTON_FILE, on?255:0);
-	err = write_int (JOGBALL_FILE, on?1:0);
+	err = write_int (TRACKBALL_FILE, on?1:0);
 	pthread_mutex_unlock (&g_lock);
 
 	return 0;
+}
+
+
+static int
+handle_trackball_light_locked(struct light_device_t* dev)
+{
+    int mode = g_attention;
+
+    if (mode == 7 && g_backlight) {
+        mode = 0;
+    }
+    LOGV("%s g_backlight = %d, mode = %d, g_attention = %d\n",
+        __func__, g_backlight, mode, g_attention);
+
+    // If the value isn't changing, don't set it, because this
+    // can reset the timer on the breathing mode, which looks bad.
+    if (g_trackball == mode) {
+        return 0;
+    }
+
+    return write_int(TRACKBALL_FILE, mode);
 }
 
 
@@ -211,12 +239,6 @@ static int set_light_battery (struct light_device_t* dev,
 	return 0;
 }
 
-static int set_light_attention (struct light_device_t* dev,
-                struct light_state_t const* state) {
-        return 0;
-}
-
-
 static int set_light_notifications (struct light_device_t* dev,
 		struct light_state_t const* state) {
 	int err =0;
@@ -228,6 +250,24 @@ static int set_light_notifications (struct light_device_t* dev,
 	return 0;
 }
 
+static int
+set_light_attention(struct light_device_t* dev,
+        struct light_state_t const* state)
+{
+    pthread_mutex_lock(&g_lock);
+    LOGV("set_light_attention g_trackball=%d color=0x%08x",
+            g_trackball, state->color);
+    if (state->flashMode == LIGHT_FLASH_HARDWARE) {
+        g_attention = state->flashOnMS;
+    } else if (state->flashMode == LIGHT_FLASH_NONE) {
+        g_attention = 0;
+    }
+    if (g_haveTrackballLight) {
+        handle_trackball_light_locked(dev);
+    }
+    pthread_mutex_unlock(&g_lock);
+    return 0;
+}
 
 static int close_lights (struct light_device_t *dev) {
 	if (dev)
@@ -279,7 +319,6 @@ static int open_lights (const struct hw_module_t* module, char const* name,
 static struct hw_module_methods_t lights_module_methods = {
 	.open = open_lights,
 };
-
 
 const struct hw_module_t HAL_MODULE_INFO_SYM = {
 	.tag = HARDWARE_MODULE_TAG,
