dcae0eaf79d9da48ecef6054bb28bb3eee830204
loxK <lox.dev@knc.nc>
2010-12-22 12:29:02 +1100

    Temporaly Temporaly disables linsensors

--- Android.mk
+++ Android.mk
@@ -19,7 +19,6 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_PATH := $(my-dir)
 subdir_makefiles := \
-	$(LOCAL_PATH)/libsensors/Android.mk \
 	$(LOCAL_PATH)/liblights/Android.mk \
 	$(LOCAL_PATH)/recovery/Android.mk
 
