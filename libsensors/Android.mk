2f691538a4b9168f57070c9eed7fbbc8b33a701f
loxK <lox.dev@knc.nc>
2010-12-24 16:19:20 +1100

    Hero has no Proximity sensor

--- libsensors/Android.mk
+++ libsensors/Android.mk
@@ -34,7 +34,6 @@ LOCAL_SRC_FILES := 						\
 				InputEventReader.cpp	\
 				SensorBase.cpp			\
 				LightSensor.cpp			\
-				ProximitySensor.cpp		\
 				AkmSensor.cpp
 				
 LOCAL_SHARED_LIBRARIES := liblog libcutils
