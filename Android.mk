f6ce6a7e44d1f7df3caf9070458b8463f24b4e9f
loxK <lox.dev@knc.nc>
2011-01-25 15:30:22 +1100

    Include libsensors makefile

--- Android.mk
+++ Android.mk
@@ -20,6 +20,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_PATH := $(my-dir)
 subdir_makefiles := \
 	$(LOCAL_PATH)/liblights/Android.mk \
+	$(LOCAL_PATH)/libsensors/Android.mk \
 	$(LOCAL_PATH)/recovery/Android.mk
 
 include $(subdir_makefiles)
